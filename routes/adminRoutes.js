83195a748daa13a654b42563770ddbe32fc5d4d9
eviloscar419-cmyk <eviloscar419@gmail.com>
2026-07-19 23:13:49 +0100

    Add route to get admin statistics

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const {
+  getAdminStats,
   getAllUsers,
   createUser,
   updateUser,
@@ -18,6 +19,7 @@ const {
 }  = require('../controllers/adminController.js');
 
 const router = express.Router();
+router.get('/stats', getAdminStats);
 router.get('/users', getAllUsers);
 router.post('/users', createUser);
 router.put('/users/:id', updateUser);
