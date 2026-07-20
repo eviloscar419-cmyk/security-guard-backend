85692522567a9b6585835ffc283410f050d77f38
eviloscar419-cmyk <eviloscar419@gmail.com>
2026-07-20 03:21:44 +0100

    Refactor chatRoutes.js for improved readability

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const { sendMessage, getMessages, getAdminConversations, markRead } = require('../controllers/chatController.js');
+
+const {
+  sendMessage,
+  getMessages,
+  getAdminConversations,
+  markRead
+} = require('../controllers/chatController.js');
 
 const router = express.Router();
+
+
 router.post('/send', sendMessage);
-router.get('/:user1/:user2', getMessages);
+
+
+// Admin route MUST come before /:user1/:user2
 router.get('/admin/:adminId', getAdminConversations);
+
+
+router.get('/:user1/:user2', getMessages);
+
+
 router.put('/mark-read/:receiverId/:senderId', markRead);
 
-module.exports =router;
+
+module.exports = router;
